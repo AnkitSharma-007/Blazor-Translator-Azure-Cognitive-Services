8eabc4bc8dc0b8ef9f2ac634222f6abd65226e98
AnkitSharma-007 <ankit.1si10cs010@gmail.com>
2020-02-09 23:28:16 +0530

    Code refactor

--- BlazorTranslator/Models/Translation.cs
+++ BlazorTranslator/Models/Translation.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace BlazorTranslator.Models
+﻿namespace BlazorTranslator.Models
 {
     public class Translation
     {
